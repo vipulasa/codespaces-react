e6a0c14a14ade50c98e6c21d6aa7dda2b356a688
Vipula S Anandapiya <v.s.anandapiya@gmail.com>
2023-02-15 20:02:52 +0000

    home template updated

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Learn React with GitHub Codespaces
           </a>
         </p>
       </header>
